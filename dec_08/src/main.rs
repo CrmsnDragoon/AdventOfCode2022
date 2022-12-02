ee257d3e096a57111443ea63efe466aaef92898f
Dylan Sinnott <crmsndragoon@gmail.com>
2022-12-02 14:38:50 +0800

    Templates

--- dec_08/src/main.rs
+++ dec_08/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    /*let (result1, result2) = depth_detector(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap());
+    println!("answer one: {}", result1);
+    println!("answer two: {}", result2);*/
+}
+
+
+#[test]
+fn test_input() {
+    /*let (result1, result2) = depth_detector(String::from_utf8(include_bytes!("../input/test_input.txt").to_vec()).unwrap());
+    assert_eq!(result1, 7);
+    assert_eq!(result2, 5);*/
 }
