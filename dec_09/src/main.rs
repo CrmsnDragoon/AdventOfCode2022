264e51d60453b82a959e37364403a505e68bd41f
Dylan Sinnott <crmsndragoon@gmail.com>
2022-12-04 22:35:28 +0800

    New answer templates

--- dec_09/src/main.rs
+++ dec_09/src/main.rs
@@ -1,13 +1,40 @@
 fn main() {
-    /*let (result1, result2) = depth_detector(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap());
-    println!("answer one: {}", result1);
-    println!("answer two: {}", result2);*/
+    println!(
+        "answer one: {}",
+        answer_one(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+    );
+    println!(
+        "answer two: {}",
+        answer_two(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+    );
 }
 
+fn answer_one(input: String) -> i32 {
+    0
+}
 
-#[test]
-fn test_input() {
-    /*let (result1, result2) = depth_detector(String::from_utf8(include_bytes!("../input/test_input.txt").to_vec()).unwrap());
-    assert_eq!(result1, 7);
-    assert_eq!(result2, 5);*/
+fn answer_two(input: String) -> i32 {
+    0
+}
+
+//This is nicer test syntax.
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            10,
+            answer_one(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            20,
+            answer_two(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+        );
+    }
 }
