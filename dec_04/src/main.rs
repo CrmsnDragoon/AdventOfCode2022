39c757bb4819c75abe274094271c9e86a005b7a1
Dylan Sinnott <crmsndragoon@gmail.com>
2022-12-04 13:42:56 +0800

    Dec 04 2: And we're done!

--- dec_04/src/main.rs
+++ dec_04/src/main.rs
@@ -3,6 +3,12 @@ use std::ops::Index;
 fn main() {
     println!(
         "answer one: {}",
+        detect_encapsulating_pairs(
+            String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap()
+        )
+    );
+    println!(
+        "answer two: {}",
         detect_overlapping_pairs(
             String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap()
         )
@@ -12,7 +18,13 @@ fn main() {
 #[test]
 fn test_input() {
     assert_eq!(
-        7,
+        2,
+        detect_encapsulating_pairs(
+            String::from_utf8(include_bytes!("../input/test_input.txt").to_vec()).unwrap()
+        )
+    );
+    assert_eq!(
+        4,
         detect_overlapping_pairs(
             String::from_utf8(include_bytes!("../input/test_input.txt").to_vec()).unwrap()
         )
@@ -20,7 +32,7 @@ fn test_input() {
     //assert_eq!(result2, 5);
 }
 
-fn detect_overlapping_pairs(input: String) -> i32 {
+fn detect_encapsulating_pairs(input: String) -> i32 {
     let mut overlapping_pair = 0;
     let pairs = input
         .lines()
@@ -48,8 +60,43 @@ fn detect_overlapping_pairs(input: String) -> i32 {
     pairs.iter().for_each(|(elf1, elf2)| {
         if elf1.0 <= elf2.0 && elf1.1 >= elf2.1 {
             overlapping_pair += 1;
+        } else if elf1.0 >= elf2.0 && elf1.1 <= elf2.1 {
+            overlapping_pair += 1;
         }
-        if elf1.0 >= elf2.0 && elf1.1 <= elf2.1 {
+    });
+    overlapping_pair
+}
+fn detect_overlapping_pairs(input: String) -> i32 {
+    let mut overlapping_pair = 0;
+    let pairs = input
+        .lines()
+        .map(|line| {
+            let elves = line
+                .split(',')
+                .map(|assignment| {
+                    let rooms = assignment
+                        .split('-')
+                        .map(|room| room.parse::<i32>().unwrap())
+                        .collect::<Vec<i32>>();
+                    let start: i32 = *rooms.index(0);
+                    let end: i32 = *rooms.index(1);
+                    (start, end)
+                })
+                .collect::<Vec<(i32, i32)>>();
+            let elf1 = elves.index(0);
+            let elf2 = elves.index(1);
+            (*elf1, *elf2)
+        })
+        .collect::<Vec<((i32, i32), (i32, i32))>>();
+    println!("pairs: {}", pairs.len());
+    pairs.iter().for_each(|(elf1, elf2)| {
+        if elf1.0 <= elf2.0 && elf1.1 >= elf2.1 {
+            overlapping_pair += 1;
+        } else if elf1.0 >= elf2.0 && elf1.1 <= elf2.1 {
+            overlapping_pair += 1;
+        } else if elf1.0 >= elf2.0 && elf1.0 <= elf2.1 {
+            overlapping_pair += 1;
+        } else if elf2.0 >= elf1.0 && elf2.0 <= elf1.1 {
             overlapping_pair += 1;
         }
     });
