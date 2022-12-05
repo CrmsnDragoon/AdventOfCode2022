051bdc9b033ee16480491a20fc1fc96dea8eb8d5
Dylan Sinnott <crmsndragoon@gmail.com>
2022-12-05 17:18:18 +0800

    Need to get the moves going

--- dec_05/src/main.rs
+++ dec_05/src/main.rs
@@ -26,31 +26,51 @@ fn answer_one<'a>(input: String) -> String {
     let mut max_column = 0;
     part_iter.next().unwrap().lines().for_each(|line| {
         let mut column = 0;
+        println!(" Reset column: {}", column);
         let mut previous_char: char = '/';
-        let mut space_count = 0;
         line.chars().for_each(|character| {
+            print!("character: {}", character);
             match character {
-                'A'..'Z' => {
+                'A'..='Z' => {
                     initial_state.insert((column, row), character);
+                    print!(" New Crate: {},{} {}", column, row, character);
                     column += 1;
+                    print!(" Increment Column: {}", column);
+                    max_column = max_column.max(column);
                 }
                 ' ' => {
                     if previous_char == ' ' || previous_char == ']' {
+                        print!(" Ignoring space");
                     } else {
                         column += 1;
+                        print!(" Increment Column: {}", column);
                     }
                 }
-                '[' => {}
-                ']' => {
-                    space_count = 0;
+                '[' => {
+                    print!(" Ignoring");
                 }
-                _ => {}
+                ']' => {
+                    print!(" Ignoring");
+                }
+                '\n' => {
+                    column += 1;
+                    print!(" Increment Column: {}", column);
+                    max_column = max_column.max(column);
+                    max_row = max_row.max(row);
+                    row = 0;
+                }
+                _ => {
+                    print!(" Ignoring");
+                }
             }
             previous_char = character;
+            println!();
         });
-        max_column = max_column.max(column);
         row += 1;
+        print!(" Increment row: {}", row);
+        max_column = max_column.max(column);
         max_row = max_row.max(row);
+        println!();
     });
 
     println!("num crates: {}", initial_state.len());
@@ -74,23 +94,29 @@ fn answer_one<'a>(input: String) -> String {
         .collect::<Vec<(i32, i32, i32)>>();
 
     let mut stacks: Vec<VecDeque<char>> = vec![];
-    for _ in 0..max_column {
-        stacks.push(VecDeque::<char>::with_capacity(max_row));
+    for _ in 0..=max_column {
+        stacks.push(VecDeque::<char>::with_capacity(max_row + 1));
     }
     //Need to sort before insertion, I think, otherwise the stacks are going to be backwards.
-    for ((x, y), contents) in initial_state {
-        stacks[x].push_back(contents);
-        println!("crate({},{}): {}", x + 1, y + 1, contents);
+    for y in 0..=max_row {
+        for x in 0..=max_column {
+            if initial_state.get(&(x, y)).is_some() {
+                stacks[x].push_back(*initial_state.get(&(x, y)).unwrap());
+            };
+        }
     }
 
     for _move_command in moves {}
 
     let mut answer: String = String::new();
 
-    for stack in 0..max_column {
+    for stack in 0..=max_column {
+        println!("{}", stacks[stack].iter().collect::<String>());
         let top = stacks[stack].front();
         if top.is_some() {
-            answer.push(*top.unwrap());
+            let value = *top.unwrap();
+            println!("{} top: {}", stack, &value);
+            answer.push(value);
         }
     }
 
