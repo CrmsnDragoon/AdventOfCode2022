3ad921e955d1aa4bc02b107bc84af5b518135d0f
Dylan Sinnott <crmsndragoon@gmail.com>
2022-12-06 14:51:45 +0800

    Dec 06 this looks like a job for iter().fold(||{})

--- dec_06/src/main.rs
+++ dec_06/src/main.rs
@@ -22,19 +22,41 @@ fn answer_two(input: String) -> i32 {
 mod tests {
     use super::*;
 
+    //noinspection SpellCheckingInspection
     #[test]
     fn test_part_1() {
         assert_eq!(
+            7,
+            answer_one(String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+        );
+        assert_eq!(5, answer_one(String::from("bvwbjplbgvbhsrlpgdmjqwftvncz")));
+        assert_eq!(6, answer_one(String::from("nppdvjthqldpwncqszvftbrmjlhg")));
+        assert_eq!(
             10,
-            answer_one(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+            answer_one(String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+        );
+        assert_eq!(
+            11,
+            answer_one(String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
         );
     }
 
+    //noinspection SpellCheckingInspection
     #[test]
     fn test_part_2() {
         assert_eq!(
-            20,
-            answer_two(String::from_utf8(include_bytes!("../input/input.txt").to_vec()).unwrap())
+            7,
+            answer_two(String::from("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+        );
+        assert_eq!(5, answer_two(String::from("bvwbjplbgvbhsrlpgdmjqwftvncz")));
+        assert_eq!(6, answer_two(String::from("nppdvjthqldpwncqszvftbrmjlhg")));
+        assert_eq!(
+            10,
+            answer_two(String::from("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+        );
+        assert_eq!(
+            11,
+            answer_two(String::from("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
         );
     }
 }
